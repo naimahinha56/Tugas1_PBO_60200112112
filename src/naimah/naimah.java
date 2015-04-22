4e85d129610f74d7dc243bc51314dc5dd87c4d86
naimahinha56 <60200112112>
2015-04-22 14:27:12 +0800

    versi 1.0.0

---
+++ src/naimah/naimah.java
@@ -0,0 +1,42 @@
+/**
+ * 
+ */
+package naimah;
+
+/**
+ * @author acer
+ *
+ */
+public class naimah {
+
+	/**
+	 * @param args
+	 */
+	
+	static String pertemuan1 = "Print";
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String praktikum1 = "BIODATA";
+		System.out.println(praktikum1);
+		biodata();
+		perkalian();
+
+	}
+	
+	public static void biodata()
+	{
+		System.out.println("Naimah");
+		System.out.println("60200112112");
+		System.out.println("KelasC");
+	}
+	
+	public static void perkalian()
+	{
+		int a = 5*6;
+		double b = 5.9*8;
+		System.out.println("PERKALIAN");
+		System.out.println("Hasilnya Adalah:"+" "+a);
+		System.out.println("Hasilnya Adalah:"+" "+b);
+	}
+
+}
